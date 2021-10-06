0af17ab757097805b8b466863c06346eaf0ac2d2
Tarun Gupta <the.tarun.gupta.raja@gmail.com>
2021-10-06 22:14:46 +0530

    Update and rename largest.cpp to sortArrayUsingSTL.cpp

--- sortArrayUsingSTL.cpp
+++ sortArrayUsingSTL.cpp
@@ -9,10 +9,7 @@ int arr[n];
 for(int i=0;i<n;i++){
 	cin>>arr[i];}
 sort(arr,arr+n);
-	// largest no.
-//	cout<<arr[n-1]<<endl;
-	//second largest no
-//	cout<<arr[n-2]<<endl;
+	
 for(int i=0;i<n;i++){
 	cout<<arr[i]<<" ";}
 return 0;
