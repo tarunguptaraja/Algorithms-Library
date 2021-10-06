aea9226a0a1cf97175d0c5beaf1c2e2eedb9688c
Vartika Gond <vartika1617@gmail.com>
2021-10-07 00:56:59 +0530

    matrix multiplication

---
+++ matrix-multiplication.cpp
@@ -0,0 +1,52 @@
+//Program to find matric multiplication
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int a[3][3];
+int b[3][3];
+int mul[3][3];
+int r,c,i,j,k;    
+cout<<"enter number of row=";    
+cin>>r;    
+cout<<"enter number of column=";    
+cin>>c;    
+cout<<"enter first matrix element=\n";    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+cin>>a[i][j];  
+}    
+}    
+cout<<"enter second matrix element=\n";    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+cin>>b[i][j];    
+}    
+}    
+cout<<"multiply of the matrix=\n";    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+mul[i][j]=0;    
+for(k=0;k<c;k++)    
+{    
+mul[i][j]+=a[i][k]*b[k][j];    
+}    
+}    
+}    
+//for printing result    
+for(i=0;i<r;i++)    
+{    
+for(j=0;j<c;j++)    
+{    
+cout<<mul[i][j]<<" ";    
+}    
+cout<<"\n";    
+}    
+return 0;  
+}    
