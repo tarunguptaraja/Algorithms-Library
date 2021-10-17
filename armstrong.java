727b50eb4e0d6771f7cc749fd1fc66af255f7eb1
Vaibhav Kumar <60957669+vkvbit@users.noreply.github.com>
2021-10-17 13:21:27 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class ArmstsrongNumber { 
+    static boolean isArmstrong(int n) {
+        int temp, digits = 0, last = 0, sum = 0;
+        temp = n;
+        while (temp > 0) {
+            temp = temp / 10;
+            digits++;
+        }
+        temp = n;
+        while (temp > 0) {
+            last = temp % 10;
+            sum += (Math.pow(last, digits));
+            temp = temp / 10;
+        }
+        if (n == sum) 
+            return true;
+        else return false;
+    }
+
+    public static void main(String args[]) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = sc.nextInt();
+        if (isArmstrong(num)) {
+            System.out.print("It is Armstrong number.");
+        } else {
+            System.out.print("It is not an Armstrong number.");
+        }
+    }
+}
